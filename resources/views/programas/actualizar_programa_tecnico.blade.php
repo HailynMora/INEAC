47cdede6a1b526d308ad43c44186ebef5725aa8b
Hailyn Mora <hailinmora.1897@gmail.com>
2022-04-24 13:54:50 -0500

    acordion en el editar tecnico

--- resources/views/programas/actualizar_programa_tecnico.blade.php
+++ resources/views/programas/actualizar_programa_tecnico.blade.php
@@ -4,37 +4,66 @@
  <h3>Actualizar Programa Técnico</h3>
 </div>
 <br><br>
-<div class="container">
-<form action="{{route('actualizar_programa_tec',$prog[0]->id)}}" method="post">
-    @csrf
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <label for="nombre">Nombre</label>
-            <input type="text" class="form-control" id="nombre" name="nombre" required value="{{$prog[0]->nombretec}}">
+<div class="accordion" id="accordionExample">
+    <div class="card">
+      <div class="card-header" id="headingOne">
+        <!---->
+        <div class="row">
+           <div class="col-md-10">
+              <h2 class="mb-0">
+                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  <i class="fas fa-edit"></i> Actualizar Programa Técnico 
+                </button>
+              </h2>
+          </div>
+          <div class="col-md-2">
+              <h2 class="mb-0">
+                <a class="btn btn-link btn-block text-left float-right" type="button" href="/programas/reporte_programas_tecnicos">
+                <i class="fas fa-arrow-circle-left"></i> Volver
+                </a>
+              </h2>
+          </div>
+      </div>
+      <!------>
+      </div>
+  
+      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+        <div class="card-body">
+            <form action="{{route('actualizar_programa_tec',$prog[0]->id)}}" method="post">
+                @csrf
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" required value="{{$prog[0]->nombretec}}">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="codigo">Codigo</label>
+                        <input type="text" class="form-control" id="codigo" name="codigo" required  value="{{$prog[0]->codigotec}}">
+                    </div>
+                </div>
+                <div class="form-row">
+                     <div class="form-group col-md-6">
+                        <label for="descripcion">Descripcion</label>
+                        <input type="text" class="form-control" id="descripcion" name="descripcion" required value="{{$prog[0]->descripcion}}">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="estado">Estado</label>
+                        <select id="estado" class="form-control" name="estado" required>
+                        <option value="{{$prog[0]->id_estado}}" selected>{{$prog[0]->estado}}</option>
+                        @foreach($estado as $d)
+                            <option value="{{$d->id}}">{{$d->descripcion}}</option>
+                        @endforeach
+                        </select>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-success">Actualizar</button>
+                <a href="{{route('reporte_tecnico')}}"type="submit" class="btn btn-warning">Cancelar</a>
+            </form>
         </div>
-        <div class="form-group col-md-6">
-            <label for="codigo">Codigo</label>
-            <input type="text" class="form-control" id="codigo" name="codigo" required  value="{{$prog[0]->codigotec}}">
-        </div>
+      </div>
     </div>
-    <div class="form-row">
-         <div class="form-group col-md-6">
-            <label for="descripcion">Descripcion</label>
-            <input type="text" class="form-control" id="descripcion" name="descripcion" required value="{{$prog[0]->descripcion}}">
-        </div>
-        <div class="form-group col-md-6">
-            <label for="estado">Estado</label>
-            <select id="estado" class="form-control" name="estado" required>
-            <option value="{{$prog[0]->id_estado}}" selected>{{$prog[0]->estado}}</option>
-            @foreach($estado as $d)
-                <option value="{{$d->id}}">{{$d->descripcion}}</option>
-            @endforeach
-            </select>
-        </div>
-    </div>
-    <button type="submit" class="btn btn-success">Actualizar</button>
-    <a href="{{route('reporte_tecnico')}}"type="submit" class="btn btn-warning">Cancelar</a>
-    
-    </form>
 </div>
+
+
+
 @endsection
