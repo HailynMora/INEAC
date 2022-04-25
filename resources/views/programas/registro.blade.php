f2b419552b81b1f534c09aa59f1a94ab721bd24f
Hailyn Mora <hailinmora.1897@gmail.com>
2022-04-25 13:42:29 -0500

    volver en los registros programas

--- resources/views/programas/registro.blade.php
+++ resources/views/programas/registro.blade.php
@@ -7,11 +7,24 @@
 <div class="accordion" id="accordionExample">
     <div class="card">
       <div class="card-header" id="headingOne">
-        <h2 class="mb-0">
-          <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            <i class="fas fa-edit"></i> Datos De Programa
-          </button>
-        </h2>
+        <!---->
+        <div class="row">
+           <div class="col-md-10">
+              <h2 class="mb-0">
+                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  <i class="fas fa-edit"></i> Datos De Programa
+                </button>
+              </h2>
+          </div>
+          <div class="col-md-2">
+              <h2 class="mb-0">
+                <a class="btn btn-link btn-block text-left float-right" type="button" href="/programas/reporte_programas">
+                <i class="fas fa-arrow-circle-left"></i> Volver
+                </a>
+              </h2>
+          </div>
+        </div>
+        <!------>
       </div>
   
       <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
